57c8b814fc7846ba112ec1f46a8b402720164ee5
Bo <bo.xing@telus.com>
2014-11-29 22:10:42 -0800

    login credential change

--- js/modules/cms/controllers/login-ctrl.js
+++ js/modules/cms/controllers/login-ctrl.js
@@ -4,10 +4,7 @@
 angular.module('CMS').controller('LoginCtrl', ['$scope', 'User', '$location', 'Auth', '$state', LoginCtrl]);
 
 function LoginCtrl($scope, User, $location, Auth, $state) {
-    $scope.credentials = {
-        "email": "xingbo828@gmail.com",
-        "password": "123456"
-    };
+    $scope.credentials = {};
     $scope.login = function () {
         $scope.loginResult = User.login($scope.credentials)
         .$promise.then(function(user){
