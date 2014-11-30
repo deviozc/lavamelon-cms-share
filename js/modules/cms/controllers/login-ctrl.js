b007fca3588a2fa8ed9c2a312fa6fc8dbd71fd73
xingbo828 <xingbo828@gmail.com>
2014-11-30 07:04:30 +0000

    login form redone

--- js/modules/cms/controllers/login-ctrl.js
+++ js/modules/cms/controllers/login-ctrl.js
@@ -1,16 +1,21 @@
 /**
  * Master Controller
  */
-angular.module('CMS').controller('LoginCtrl', ['$scope', 'User', '$location', 'Auth', '$state', LoginCtrl]);
+angular.module('CMS').controller('LoginCtrl', ['$scope', 'User', '$location', 'Auth', '$state', 'toaster' , LoginCtrl]);
 
-function LoginCtrl($scope, User, $location, Auth, $state) {
+function LoginCtrl($scope, User, $location, Auth, $state, toaster) {
     $scope.credentials = {};
     $scope.login = function () {
         $scope.loginResult = User.login($scope.credentials)
-        .$promise.then(function(user){
+        .$promise.then(
+            function(user){
             Auth.setCurrentUser(user);
             $state.go('main.dashboard');
-        });
+        },function(error){
+            toaster.pop('error', 'Login Failed', 'Please check your email and password.');
+            console.log(error);
+        }
+        );
         
         //console.log($scope.loginResult);
     };
